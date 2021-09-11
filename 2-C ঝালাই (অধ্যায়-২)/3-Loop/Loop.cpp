6ac32dfb18d892256499b398f51d8aea886e931f
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-09-11 20:11:38 +0600

    Update Loop.cpp

--- 2-C ঝালাই (অধ্যায়-২)/3-Loop/Loop.cpp
+++ 2-C ঝালাই (অধ্যায়-২)/3-Loop/Loop.cpp
@@ -1,4 +1,3 @@
-//Loop
 //counts how many 2 divides 100
 int x=100,count=0;
 while (x%2==0){
@@ -10,27 +9,32 @@ while (x%2==0){
 //C
 int a;
 while (scanf("%d",&a)!=EOF){
+if(a==0){
+break;
+    }
+printf("%d\n",a);
+}
+
+int a;
+while (scanf("%d",&a)!=EOF&& a){    
+    printf("%d\n",a);
+}
+
+//C++
+int a;
+while (cin>>a){
     if(a==0){
         break;
     }
-    printf("%d\n",a);
-}
+    cout<<a<<endl;
+ }
 
-int a;
-while (scanf("%d",&a)!=EOF && a){
 
-    printf("%d\n",a);
-}
-//C++
 int a;
 while (cin>>a && a){
     cout<<a<<endl;
 }
 
-int a;
-while (cin>>a && a){
-    cout<<a<<endl;
-}
 //1+ (1+2) + (1+2+3) +…. + (1+2+3+…+n)
 
 int n,sum=0;
@@ -43,14 +47,3 @@ for(int i=1;i<=n;i++){
     }
     cout<<sum<<endl;
 }
-
-
-//Again
-    int n,sum=0;
-    cin>>n;
-
-    for(int i=1;i<=n;i++){
-        cout<<"Sum of "<<i<<":";
-        sum+=(i*(i+1))/2;
-        cout<<sum<<endl;
-    }
